626c9460f66b8b29a78cf49d00e2e082c6fa2b86
Kevin Williams <kevcoder@gmail.com>
2023-03-09 22:29:52 -0600

    override appsettings.json with environment variables

--- Program.cs
+++ Program.cs
@@ -4,19 +4,35 @@ var corsNamedPolicy = "policy";
 
 var builder = WebApplication.CreateBuilder(args);
 
+var allowedOrigins = builder.Configuration["AllowedOrigins"];
+if (string.IsNullOrWhiteSpace(allowedOrigins))
+{
+    allowedOrigins = Environment.GetEnvironmentVariable("MARSHA_API_ALLOWED_ORIGINS") ?? string.Empty;
+}
+
+var apiKey = builder.Configuration["Stripe:ApiKey"];
+if (string.IsNullOrWhiteSpace(apiKey))
+{
+    StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("MARSHA_SITE_STRIPE_KEY") ?? string.Empty;
+}
+
 // Add services to the container.
 builder.Services.AddCors(opt =>
 {
-    opt.AddPolicy(corsNamedPolicy,
-    policy =>
-    {
-        var AllowedOrigins = (builder.Configuration["AllowedOrigins"] ?? "").Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-        policy.WithOrigins(AllowedOrigins)
-        .AllowAnyHeader()
-        .AllowAnyMethod();
 
-    });
+    if (!string.IsNullOrWhiteSpace(allowedOrigins))
+    {
+        opt.AddPolicy(corsNamedPolicy,
+        policy =>
+        {
+            var AllowedOrigins = allowedOrigins.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            policy.WithOrigins(AllowedOrigins)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+    }
 });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -30,8 +46,6 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
-StripeConfiguration.ApiKey = builder.Configuration["Stripe:ApiKey"].ToString();
 app.UseHttpsRedirection();
 
 app.UseCors(corsNamedPolicy);
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # proxy service for calling Stripe checkout
 ## Expectations
 1. Environment Variables
-    1. MARSHASTRIPEAPI_ALLOWEDORIGINS = csv or origins like "https://localhost:4200,http://localhost:4200"
+    1. (REQUIRED) MARSHA_SITE_STRIPE_KEY = the Stripe api key
+    1. (OPTIONAL) MARSHA_API_ALLOWED_ORIGINS = csv of origins like "https://localhost:4200,http://localhost:4200"
 
 ## Docker
 1. docker build . -t kevcoder/marsha-stripe-api
